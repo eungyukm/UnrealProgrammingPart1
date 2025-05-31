d8ff19cfad390a112da5b7c503a663a37a9ceb1b
eungyu kim <86348187+eungyukm@users.noreply.github.com>
2025-05-31 20:18:00 +0900

    feat: upload project

---
+++ Project/Source/Project/MyOject.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyOject.h"
+
+void UMyOject::PrintHello()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Hello from UMyOjbect!"));
+}
---
+++ Project/Source/Project/MyOject.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "MyOject.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_API UMyOject : public UObject
+{
+	GENERATED_BODY()
+
+	public:
+	void PrintHello();
+};
